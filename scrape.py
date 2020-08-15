71f8f823e2bec3686609e07829f8c4ddf25e0e2b
pradityabagus <praditya@ehealth.co.id>
2020-08-15 20:34:51 +0700

    bs4 assignments

--- scrape.py
+++ scrape.py
@@ -7,11 +7,15 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = "http://py4e-data.dr-chuck.net/comments_42.html"
+url = "http://py4e-data.dr-chuck.net/comments_887473.html"
 page = urlopen(url,context=ctx).read()
 
 souped = bs(page,"html.parser")
-linktag = souped.selec
+linktag = souped.find_all('span', class_='comments')
+
+sum = 0
 
 for link in linktag:
-    print(link)
+    sum += int(link.contents[0])
+
+print(sum)
